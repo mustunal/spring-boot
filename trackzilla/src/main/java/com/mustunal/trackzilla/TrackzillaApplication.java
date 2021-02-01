d3c0cfacb0538169cd45272f479f32b82d71f6a4
mustunal <mustunal@thy.com>
2021-02-01 13:07:02 +0300

    trackzilla project added.

---
+++ trackzilla/src/main/java/com/mustunal/trackzilla/TrackzillaApplication.java
@@ -0,0 +1,13 @@
+package com.mustunal.trackzilla;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackzillaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrackzillaApplication.class, args);
+	}
+
+}
---
+++ trackzilla/src/test/java/com/mustunal/trackzilla/TrackzillaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mustunal.trackzilla;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrackzillaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
